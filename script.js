acf84e214d4824f4231734944df65664908c92d1
rushi-reactdev <rushikeshkale669@gmail.com>
2023-04-04 23:37:22 +0530

    I HAVE CREATED SOME IF ELSE STATEMENT OF JAVASCRIPTS

---
+++ script.js
@@ -0,0 +1,43 @@
+// if else statements
+// example one
+let age = 17;
+if(age>=18){
+    console.log("can applhy for driving liscence")
+}
+else{
+    console.log ("user cannot apply for driving liscence")
+}
+
+// example number two
+let marks = 89;
+if(marks>90){
+    console.log("CONGRATS YOU ACHIVE A+ GRADE IN EXAMINATION")
+}
+else(
+    console.log('YOU ACHIVE A GRADE IN EXAMINATION')
+)
+// example no 3
+let user='have subscription';
+if(user= 'have subscription')
+{
+    console.log('YOU CAN WATCH LIVE MATCHES')
+}
+else{
+    ('PLEASE SUBSCRIBE TO WATCH LIVE MATCHES')
+}
+// example no 4
+let vehiclespeed = "60mph";
+if(vehiclespeed>"55mph"){
+console.log('vehicle is out of economy')
+}
+else{
+    console.log("vehicle is under economy")
+}
+// example no 5 
+let player = 80% "fit";
+if(player=100%"fit"){
+    console.log("player can play the league matches")
+}
+else{
+    console.log('player must need to rest till he get fit')
+}
